92746c6e461f63525ea3d3f7fca206ecfc74d467
Cruz Nazareno <cruznazareno05@gmail.com>
2024-07-07 08:38:57 -0300

    Agregar archivos de proyecto.

---
+++ Calculadora basica/Calculadora.cs
@@ -0,0 +1,40 @@
+﻿namespace Calculadora_basica
+{
+    internal class Calculadora
+    {
+        public Calculadora() 
+        {
+            
+        }
+
+        // Metodos
+        public static int Sumar(int numero1, int numero2)
+        {
+            int sumarRetorno = numero1 + numero2;
+
+            return sumarRetorno;
+        }
+
+        public static int Restar(int numero1, int numero2)
+        {
+            int restaRetorno = numero1 - numero2;
+
+            return restaRetorno;
+        }
+
+        public static int Multiplicar(int numero1, int numero2)
+        {
+            int multiplicarRetorno = numero1 * numero2;
+
+            return multiplicarRetorno;
+        }
+
+        public static double Dividir(double numero1, double numero2)
+        {
+            double divisionRetorno = numero1 / numero2;
+
+            return divisionRetorno;
+        }
+        // TODO: hacer los demas metodos, luego agregar validaciones y hacer un obtenerOpcionValida en main
+    }
+}
---
+++ Calculadora basica/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Calculadora_basica
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Ejercicio: Calculadora Básica con POO
+
+            Objetivo:
+            Crear una calculadora básica utilizando los principios de la Programación Orientada a Objetos (POO) en C#. 
+            La calculadora debe ser capaz de realizar las cuatro operaciones aritméticas fundamentales: 
+            suma, resta, multiplicación y división.
+
+
+            Consideraciones adicionales:
+            - Validar la entrada del usuario para asegurarse de que ingresa números válidos.
+            - Manejar posibles excepciones que puedan ocurrir durante la ejecución de las operaciones aritméticas, 
+              especialmente la división por cero.
+            - Escribir comentarios adecuados en el código para mejorar la legibilidad y el mantenimiento.
+
+            Ejemplo de ejecución:
+            -------------------------
+            Ingrese el primer número: 10
+            Ingrese el segundo número: 5
+            Seleccione la operación (suma, resta, multiplicación, división): suma
+            Resultado: 15
+            ¿Desea realizar otra operación? (s/n): s
+            Ingrese el primer número: 20
+            Ingrese el segundo número: 4
+            Seleccione la operación (suma, resta, multiplicación, división): división
+            Resultado: 5
+            ¿Desea realizar otra operación? (s/n): n
+            -------------------------
+            ¡Gracias por usar la calculadora!
+            */
+
+            Calculadora calculadora = new();
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
